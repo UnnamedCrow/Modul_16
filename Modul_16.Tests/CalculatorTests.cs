0c48f3e5fd3d63ba4e759d628e3f09a9757334ca
Максим Самисько <masechk@gmail.com>
2023-05-29 22:49:58 +0300

    Unit_16 - Try make a NUnit and XUnit tests

---
+++ Modul_16.Tests/CalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace Modul_16.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(100 == 100);
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.False(100 == 1001);
+        }
+        [Test]
+        public void Substruction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Substruction(300,10) == 290);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Division(10,3) == 3);
+        }
+        [Test]
+        public void Division_MustThrowExeption()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+    }
+}
---
+++ Modul_16.Tests_1/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using Xunit;
+
+namespace Modul_16.Tests_1
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Substruction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Substruction(100,50) == 50);
+        }
+        [Fact]
+        public void Division_ReturnsCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Division(10,3) == 3);
+        }
+        [Fact]
+        public void Division_MustThrowExeption()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(10, 0));
+        }
+    }
+}
