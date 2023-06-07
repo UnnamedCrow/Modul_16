b4712bdf62560b9a840632ed2671d20054bc0d13
Максим Самисько <masechk@gmail.com>
2023-06-07 21:18:49 +0300

    Unit_17-Homework: Added First refactoring variant

---
+++ Unit_17-Homework(Refactoring)/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_17_Homework_Refactoring_
+{
+    internal class Calculator
+    {
+    }
+}
---
+++ Unit_17-Homework(Refactoring)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Unit_17_Homework_Refactoring_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
